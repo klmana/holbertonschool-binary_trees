cab73aa5c2a52a07901fd46bd5bd218e43c3ea69
RubberizedDuck <dyl_2536@hotmail.com>
2022-08-30 19:01:21 -0700

    Add comments for code

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - creates a node to start a binary tree
+ * @parent: the root of the tree
+ * @value: the value to be stored in each node
+ * Return: the new node in binary tree, or NULL on failure
+ */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
