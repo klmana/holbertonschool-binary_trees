b459fad17ab5d4476a5544a47b2d655282e54706
RubberizedDuck <dyl_2536@hotmail.com>
2022-08-30 19:14:17 -0700

    Fix: betty coding style indentation

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,23 +9,19 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-        binary_tree_t *new;
+	binary_tree_t *new;
 
-        if (parent == NULL)
-        {
-                return (NULL);
-        }
+	if (parent == NULL)
+		return (NULL);
 
-        new = binary_tree_node(parent, value);
-        if (new == NULL)
-        {
-                return (NULL);
-        }
-        new->right = parent->right;
-        if (new->right != NULL)
-        {
-                new->right->parent = new;
-        }
-        parent->right = new;
-        return (new);
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
+
+	new->right = parent->right;
+	if (new->right != NULL)
+		new->right->parent = new;
+
+	parent->right = new;
+	return (new);
 }
