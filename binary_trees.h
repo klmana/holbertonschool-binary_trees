25e4d69463058bb076a640fff4e5880cca9ec66d
root <km@rtin.mobi>
2022-08-31 02:16:57 -0700

    task7

--- binary_trees.h
+++ binary_trees.h
@@ -29,7 +29,6 @@ typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
 
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -68,6 +67,9 @@ int binary_tree_is_heap(const binary_tree_t *tree);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
+int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
+size_t _height(const binary_tree_t *tree);
+void binary_tree_print(const binary_tree_t *tree);
 
 #endif
 
