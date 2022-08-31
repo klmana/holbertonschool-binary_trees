d82fe59942dbf3e3eea89626d18103da43790f3c
root <km@rtin.mobi>
2022-08-31 02:39:33 -0700

    fix proto task7

--- binary_trees.h
+++ binary_trees.h
@@ -67,8 +67,6 @@ int binary_tree_is_heap(const binary_tree_t *tree);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
-static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
-static size_t _height(const binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *tree);
 
 #endif
